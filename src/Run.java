884bfb2cb0ba1c09aa51cff0b66d52da7c538012
Aurora Esteban Toscano <aestebant@uco.es>
2019-10-29 14:25:18 +0100

    Solving problemas with Run.

--- src/Run.java
+++ src/Run.java
@@ -1,49 +1,97 @@
-import algorithms.MISimpleKMeans;
 import evaluators.ClusterEvaluation;
 import utils.ProcessDataset;
+import weka.clusterers.AbstractClusterer;
 import weka.clusterers.Clusterer;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Run {
     public static void main(String[] args) {
 
-        Clusterer clusterer = new MISimpleKMeans();
-        try {
-            ((MISimpleKMeans) clusterer).setNumClusters(2);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //MIDBSCAN clusterer = new MIDBSCAN();
-        //MIOPTICS clusterer = new MIOPTICS();
-        // E: epsion, M: min points
-        /*String clusterOptions = " -E 1.4 -M 8";
-        try {
-            clusterer.setOptions(weka.core.Utils.splitOptions(clusterOptions));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        String[] datasets = {
+                "component_relational",
+                "eastwest_relational",
+                "elephant_relational",
+                "fox_relational",
+                "function_relational",
+                "musk1_relational",
+                "musk2_relational",
+                "mutagenesis3_atoms_relational",
+                "mutagenesis3_bonds_relational",
+                "mutagenesis3_chains_relational",
+                "process_relational",
+                "suramin_relational",
+                "tiger_relational",
+                "trx_relational",
+                "westeast_relational"
+        };
 
-        try {
-            clusterer.buildClusterer(ProcessDataset.readArff("datasets/eastwest_relational-z4.arff"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(clusterer.toString());
+        String[] standardization = {
+                "",
+                "-z4",
+                "-z5"
+        };
 
-        String evalOptions = "-t datasets/component_relational.arff"
-                + " -c last";
-        try {
-            System.out.println(ClusterEvaluation.evaluateClusterer(clusterer, weka.core.Utils.splitOptions(evalOptions)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        /*ClusterEvaluation evaluation = new ClusterEvaluation();
+        String[] clustering = {
+                "MIDBSCAN",
+                "MISimpleKMeans",
+                "BAMIC"
+        };
 
-        evaluation.setClusterer(clusterer);
-        try {
-            evaluation.evaluateClusterer(data);
-        } catch (Exception e) {
-            e.printStackTrace();
+        Map<String, String> options = new HashMap<>();
+        options.put("MIDBSCAN", " -E 1.4 -M 8");
+        options.put("MISimpleKMeans", "");
+        options.put("BAMIC", "");
+
+        for (String d : datasets) {
+            for (String z : standardization) {
+                for (String c : clustering) {
+
+                    System.out.println("=========================================");
+                    System.out.println("Algorithm: " + c);
+                    System.out.println("Dataset: " + d);
+                    System.out.println("Standarization: " + z);
+                    System.out.println("=========================================");
+
+                    Class<? extends AbstractClusterer> absCluster = null;
+                    try {
+                        absCluster = Class.forName("algorithms." + c).asSubclass(AbstractClusterer.class);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    assert absCluster != null;
+                    Clusterer clusterer = null;
+                    try {
+                        clusterer = absCluster.getDeclaredConstructor().newInstance();
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                    assert clusterer != null;
+
+                    try {
+                        ((algorithms.Clusterer) clusterer).setOptions(weka.core.Utils.splitOptions(options.get(c)));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        clusterer.buildClusterer(ProcessDataset.readArff("datasets/" + d + z + ".arff"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(clusterer.toString());
+
+                    String evalOptions = "-t datasets/" + d + z + ".arff -c last";
+                    try {
+                        System.out.println(ClusterEvaluation.evaluateClusterer(clusterer, weka.core.Utils.splitOptions(evalOptions)));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
         }
-        System.out.println(evaluation.clusterResultsToString());*/
     }
 }
