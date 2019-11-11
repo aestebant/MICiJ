e17731dd9a97212515a1dfecf6df1b6ea3b972ce
Aurora Esteban Toscano <aestebant@uco.es>
2019-11-11 01:33:00 +0100

    Create a external method for extract instances from a bag.

--- src/utils/ProcessDataset.java
+++ src/utils/ProcessDataset.java
@@ -1,11 +1,14 @@
 package utils;
 
+import weka.core.Attribute;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ProcessDataset {
     public static Instances readArff(String path) {
@@ -17,7 +20,27 @@ public class ProcessDataset {
             e.printStackTrace();
         }
         assert arff != null;
-        Instances data = arff.getData();
-        return data;
+        return arff.getData();
+    }
+
+    public static Instances extractInstances(Instance bag) {
+        boolean isBag = false;
+        try {
+            isBag = bag.attribute(1).isRelationValued();
+        } catch (Exception ignored) {
+            //Capturar excepción Instance doesn't have access to a dataset e ignorarla.
+        }
+
+        if (isBag)
+            return new Instances(bag.relationalValue(1));
+        else {
+            ArrayList<Attribute> attributes = new ArrayList<>(bag.numAttributes());
+            for (int i = 0; i < bag.numAttributes(); ++i) {
+                attributes.add(new Attribute("att" + i));
+            }
+            Instances instances = new Instances("aux", attributes, 1);
+            instances.add(bag);
+            return instances;
+        }
     }
 }
