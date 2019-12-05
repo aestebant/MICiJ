8f4a6f99f703d87d54c8e09ccb4b756e9d1bc8bb
Aurora Esteban Toscano <aestebant@uco.es>
2019-12-05 22:02:39 +0100

    Previous version didnt work with deep copies of data, but with references. Also, adding integration with apache math.

--- src/main/java/miclustering/RunPreprocess.java
+++ src/main/java/miclustering/RunPreprocess.java
@@ -8,37 +8,45 @@ import weka.core.converters.ConverterUtils;
 public class RunPreprocess {
     public static void main(String[] args) {
         String[] datasets = {
-                "component_relational",
-                "eastwest_relational",
-                "elephant_relational",
-                "fox_relational",
-                "function_relational",
-                "musk1_relational",
-                "musk2_relational",
-                "mutagenesis3_atoms_relational",
-                "mutagenesis3_bonds_relational",
-                "mutagenesis3_chains_relational",
-                "process_relational",
-                "suramin_relational",
-                "tiger_relational",
-                "trx_relational",
-                "westeast_relational"
+//                "component_relational",
+//                "eastwest_relational",
+//                "elephant_relational",
+//                "fox_relational",
+//                "function_relational",
+//                "musk1_relational",
+//                "musk2_relational",
+//                "mutagenesis3_atoms_relational",
+//                "mutagenesis3_bonds_relational",
+//                "mutagenesis3_chains_relational",
+//                "process_relational",
+//                "suramin_relational",
+//                "tiger_relational",
+//                "trx_relational",
+//                "westeast_relational"
+                "animals_relational"
         };
 
         MIStandardization filter = new MIStandardization();
         for (String d : datasets) {
-            Instances data = ProcessDataset.readArff("datasets/" + d + ".arff");
-
-            Instances dataZ4 = filter.z4(data);
-            Instances dataZ5 = filter.z5(data);
-
-            try {
-                ConverterUtils.DataSink.write("datasets/" + d + "-z4.arff", dataZ4);
-                ConverterUtils.DataSink.write("datasets/" + d + "-z5.arff", dataZ5);
-            } catch (Exception e) {
-                e.printStackTrace();
+            for (int i = 0; i < 2; ++i) {
+                Instances data = ProcessDataset.readArff("datasets/" + d + ".arff");
+                String ext = "";
+                switch (i) {
+                    case 0:
+                        filter.z1(data);
+                        ext = "-z1";
+                        break;
+                    case 1:
+                        filter.z5(data);
+                        ext = "-z5";
+                        break;
+                }
+                try {
+                    ConverterUtils.DataSink.write("datasets/" + d + ext + ".arff", data);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-
             System.out.println("Finished dataset " + d);
         }
     }
--- src/main/java/miclustering/filters/MIStandardization.java
+++ src/main/java/miclustering/filters/MIStandardization.java
@@ -1,78 +1,85 @@
 package miclustering.filters;
 
+import miclustering.utils.ProcessDataset;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+import org.apache.commons.math3.stat.descriptive.rank.Max;
+import org.apache.commons.math3.stat.descriptive.rank.Min;
 import weka.core.Instance;
 import weka.core.Instances;
 
 public class MIStandardization {
 
-    private Instances dataset;
+    private double[] min;
+    private double[] max;
+    private double[] mean;
+    private double[] std;
 
-    public Instances z4(Instances dataset) {
-        this.dataset = new Instances(dataset);
-        int numAtt = dataset.get(0).relationalValue(1).numAttributes();
-        for (int att = 0; att < numAtt; ++att) {
-            double minValue = getMin(att);
-            double maxValue = getMax(att);
-            for (Instance bag : this.dataset) {
-                Instances instances = bag.relationalValue(1);
-                for (Instance i : instances) {
-                    if (minValue == maxValue)
-                        i.setValue(att, 0D);
-                    else {
-                        double standardized = i.value(att) / (maxValue - minValue);
-                        i.setValue(att, standardized);
-                    }
+    private void preparation(Instances dataset) {
+        int nAtt = dataset.get(0).relationalValue(1).numAttributes();
+        int nInst = 0;
+        for (Instance i : dataset) {
+            nInst += i.relationalValue(1).numInstances();
+        }
+
+        double[][] arrayData = new double[nInst][nAtt];
+        int k = 0;
+        for (Instance i : dataset) {
+            for (Instance j : i.relationalValue(1)) {
+                arrayData[k] = ProcessDataset.instanceToArray(j);
+                k++;
+            }
+        }
+        RealMatrix data = new Array2DRowRealMatrix(arrayData);
+
+        min = new double[nAtt];
+        max = new double[nAtt];
+        mean = new double[nAtt];
+        std = new double[nAtt];
+        for (int i = 0; i < nAtt; ++i) {
+            Min computeMin = new Min();
+            min[i] = computeMin.evaluate(data.getColumn(i));
+            Max computeMax = new Max();
+            max[i] = computeMax.evaluate(data.getColumn(i));
+            Mean computeMean = new Mean();
+            mean[i] = computeMean.evaluate(data.getColumn(i));
+            StandardDeviation computeStd = new StandardDeviation();
+            std[i] = computeStd.evaluate(data.getColumn(i));
+        }
+    }
+
+    public Instances z1(Instances dataset) {
+        preparation(dataset);
+        for (Instance bag : dataset) {
+            for (Instance instance : bag.relationalValue(1)) {
+                for (int i = 0; i < instance.numAttributes(); ++i) {
+                    double val = (instance.value(i) - mean[i]) / std[i];
+                    if (Double.isNaN(val))
+                        instance.setValue(i, 0);
+                    else
+                        instance.setValue(i, val);
                 }
             }
         }
-        return this.dataset;
+        return dataset;
     }
 
     public Instances z5(Instances dataset) {
-        this.dataset = new Instances(dataset);
-        int numAtt = dataset.get(0).relationalValue(1).numAttributes();
-        for (int att = 0; att < numAtt; ++att) {
-            double minValue = getMin(att);
-            double maxValue = getMax(att);
-            //System.out.println("Minimal: " + minValue + " Maximal: " + maxValue);
-            for (Instance bag : this.dataset) {
-                Instances instances = bag.relationalValue(1);
-                for (Instance i : instances) {
-                    if (minValue == maxValue)
-                        i.setValue(att, 0D);
-                    else {
-                        double standardized = (i.value(att) - minValue) / (maxValue - minValue);
-                        i.setValue(att, standardized);
-                    }
+        preparation(dataset);
+        for (Instance bag : dataset) {
+            for (Instance instance : bag.relationalValue(1)) {
+                for (int i = 0; i < instance.numAttributes(); ++i) {
+                    double val = (instance.value(i) - min[i]) / (max[i] - min[i]);
+                    if (Double.isNaN(val))
+                        instance.setValue(i, 0);
+                    else
+                        instance.setValue(i, val);
                 }
             }
         }
-        return this.dataset;
+        return dataset;
     }
 
-    private double getMin(int attIdx) {
-        double min = dataset.get(0).relationalValue(1).get(0).value(attIdx);
-        for (Instance bag : dataset) {
-            Instances instances = bag.relationalValue(1);
-            for (Instance i : instances) {
-                if (i.value(attIdx) < min) {
-                    min = i.value(attIdx);
-                }
-            }
-        }
-        return min;
-    }
-
-    private double getMax(int attIdx) {
-        double max = dataset.get(0).relationalValue(1).get(0).value(attIdx);
-        for (Instance bag : dataset) {
-            Instances instances = bag.relationalValue(1);
-            for (Instance i : instances) {
-                if (i.value(attIdx) > max) {
-                    max = i.value(attIdx);
-                }
-            }
-        }
-        return max;
-    }
 }
