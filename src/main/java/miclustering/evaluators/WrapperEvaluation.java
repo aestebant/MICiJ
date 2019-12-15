17d90201c0860aecf5cd62b60d2807182e22d8c5
Aurora Esteban Toscano <aestebant@uco.es>
2019-12-15 02:27:23 +0100

    Changed geometric mean to average in macro-metrics.

--- src/main/java/miclustering/evaluators/WrapperEvaluation.java
+++ src/main/java/miclustering/evaluators/WrapperEvaluation.java
@@ -2,7 +2,7 @@ package miclustering.evaluators;
 
 import miclustering.utils.PrintConfusionMatrix;
 import miclustering.utils.ProcessDataset;
-import org.apache.commons.math3.stat.descriptive.moment.GeometricMean;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import weka.core.DistanceFunction;
 import weka.core.Instances;
 import weka.core.Utils;
@@ -16,6 +16,7 @@ public class WrapperEvaluation {
     private DistanceFunction distanceFunction;
     private SilhouetteIndex silhouette;
     private S_DbwIndex sdbw;
+    private DBCV dbcv;
     private ClassEvaluation classEval;
     private int maxNumClusters;
 
@@ -28,7 +29,9 @@ public class WrapperEvaluation {
             e.printStackTrace();
         }
         sdbw = new S_DbwIndex(dataset, maxNumClusters, distanceFunction);
-        silhouette = new SilhouetteIndex(dataset, maxNumClusters, distanceFunction, Runtime.getRuntime().availableProcessors());
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        silhouette = new SilhouetteIndex(dataset, maxNumClusters, distanceFunction, nThreads);
+        dbcv = new DBCV(dataset, distanceFunction, maxNumClusters, nThreads);
         int nClass = dataset.numDistinctValues(2);
         classEval = new ClassEvaluation(dataset, maxNumClusters, nClass);
         this.maxNumClusters = maxNumClusters;
@@ -39,19 +42,24 @@ public class WrapperEvaluation {
     }
 
     public double getSilhouette(Vector<Integer> clusterAssignments) {
-        int[] instancesPerCluster = countInstancesPerCluster(clusterAssignments, maxNumClusters);
-        return silhouette.computeIndex(clusterAssignments, instancesPerCluster);
+        int[] bagsPerCluster = countBagsPerCluster(clusterAssignments, maxNumClusters);
+        return silhouette.computeIndex(clusterAssignments, bagsPerCluster);
     }
 
     public double getSdbw(Vector<Integer> clusterAssignments) {
         return sdbw.computeIndex(clusterAssignments);
     }
 
+    public double getDBCV(Vector<Integer> clusterAssignments) {
+        int[] bagsPerCluster = countBagsPerCluster(clusterAssignments, maxNumClusters);
+        return dbcv.computeIndex(clusterAssignments, bagsPerCluster);
+    }
+
     public Map<String, String> getExternalEvaluation(Vector<Integer> clusterAssignments) {
-        int[] instancesPerCluster = countInstancesPerCluster(clusterAssignments, maxNumClusters);
+        int[] bagsPerCluster = countBagsPerCluster(clusterAssignments, maxNumClusters);
         ClassEvalResult cer = null;
         try {
-            cer = classEval.computeConfusionMatrix(clusterAssignments, instancesPerCluster);
+            cer = classEval.computeConfusionMatrix(clusterAssignments, bagsPerCluster);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,14 +67,17 @@ public class WrapperEvaluation {
         double[] precision = classEval.computePrecision(cer);
         double[] recall = classEval.computeRecall(cer);
         double[] f1 = classEval.computeF1(precision, recall);
-        GeometricMean gm = new GeometricMean();
+        Mean mean = new Mean();
+        double[] weights = new double[bagsPerCluster.length];
+        for (int i = 0; i < weights.length; ++i)
+            weights[i] = (double) bagsPerCluster[i] / clusterAssignments.size();
 
         Map<String, String> result = new HashMap<>(2);
         result.put("purity", String.valueOf(classEval.computePurity(cer.getConfMatrix())));
         result.put("rand", String.valueOf(classEval.computeRandIndex(cer)));
-        result.put("macro-precision", String.valueOf(gm.evaluate(precision)));
-        result.put("macro-recall", String.valueOf(gm.evaluate(recall)));
-        result.put("macro-f1", String.valueOf(gm.evaluate(f1)));
+        result.put("macro-precision", String.valueOf(mean.evaluate(precision, weights)));
+        result.put("macro-recall", String.valueOf(mean.evaluate(recall, weights)));
+        result.put("macro-f1", String.valueOf(mean.evaluate(f1, weights)));
         result.put("micro-precision", Arrays.toString(precision));
         result.put("micro-recall", Arrays.toString(recall));
         result.put("micro-f1", Arrays.toString(f1));
@@ -74,7 +85,7 @@ public class WrapperEvaluation {
         return result;
     }
 
-    private int[] countInstancesPerCluster(Vector<Integer> clusterAssignments, int maxNumClusters) {
+    private int[] countBagsPerCluster(Vector<Integer> clusterAssignments, int maxNumClusters) {
         int[] instancesPerCluster = new int[maxNumClusters];
         for (Integer classIdx : clusterAssignments) {
             instancesPerCluster[classIdx]++;
